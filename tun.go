6ffae20d2eae370c3c1aa122b33d9429d13d47e8
nekolsd <nekolsd@proton.me>
2026-05-19 13:06:50 +0800

    Add ICMP exclude support for nftables auto-redirect

--- tun.go
+++ tun.go
@@ -102,6 +102,7 @@ type Options struct {
 	ExcludePackage                        []string
 	IncludeMACAddress                     []net.HardwareAddr
 	ExcludeMACAddress                     []net.HardwareAddr
+	ExcludeICMP                           bool
 	InterfaceFinder                       control.InterfaceFinder
 	InterfaceMonitor                      DefaultInterfaceMonitor
 	FileDescriptor                        int
